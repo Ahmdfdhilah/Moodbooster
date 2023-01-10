e8041a472a22db5f4ca451dc2f20dbbf11a1bdd8
Ahmad_Fadillah <ahmad.fadilah0210@gmail.com>
2023-01-11 01:40:57 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+onload = () => {
+  document.body.classList.remove("container");
+};
+var options = {
+  strings: ["Haii u, semoga setelah u liat ini, gaada yang ngerusak mood u lagi yaa di liburan inii, ohh iya semangatt juga remednyaa!!", "lovee u :)"],
+  typeSpeed: 40,
+  loop: false,
+  delaySpeed: 80
+};
+
+new Typed("#typed", options);
